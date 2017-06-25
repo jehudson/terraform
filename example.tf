1f028a0c222d7527016d136e0d49627f468290ad
John Hudson <john@jehudson.me>
2017-06-25 20:52:21 +0100

    add cidrs

--- example.tf
+++ example.tf
@@ -1,11 +1,12 @@
 provider "aws" {
-  access_key = ${var.access_key}"
-  secret_key = ${var.secret_key}"
-  region = "${var.region}"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "${var.region}"
 }
 
 resource "aws_eip" "ip" {
-  instance = "aws_instance.example.id}"
+  instance = "${aws_instance.example.id}"
+  vpc      = true
 }
 
 resource "aws_instance" "example" {
